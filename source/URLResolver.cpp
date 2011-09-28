40b4ed3fa5f848e99f0ecc120b246fcb4414a441
Nathan Standiford <ns@byu.edu>
2011-09-28 14:37:41 -0600

    changed http and file handlers

--- source/URLResolver.cpp
+++ source/URLResolver.cpp
@@ -15,15 +15,35 @@ const int POUND = 35;
 const int QMARK = static_cast<int>('?');
 const char NUL = '\0';
 
+char * removeFileRoot(char * url) {
+  const int FILE_SCHEME_LENGTH = 6;
+  url = url + FILE_SCHEME_LENGTH;
+  if(*(url + 1) != NUL && static_cast<int>(*(url + 1)) == SLASH) {
+    url++;
+    *url = NUL;
+  }
+  url++;
+  return url;
+    
+}
 
-char * removeRoot(char * url) {
+char * removeHttpRoot(char * url) {
   const int SCHEME_LENGTH = 7;
   url = url + SCHEME_LENGTH;
-  while (static_cast<int>(*url) != SLASH)
+  while ((*url) != NUL && static_cast<int>(*url) != SLASH)
     ++url;
-  *url = NUL;
-  ++url;
+  if ((*url) != NUL) {
+    *url = NUL;
+    ++url;
+  }
   return url;
+}
+
+char * removeRoot(char * url) {
+  if((*url) == 'f')
+    return removeFileRoot(url);
+  else
+    return removeHttpRoot(url);
 }
   
 void parent(char * url) {
@@ -85,7 +105,7 @@ char * resolveUrl(char * url, char * relUrl) {
 
 int main(int argc, char ** argv) {
   if (argc != 3) {
-    cout << "Illegal operation!"
+    cout << "Usage: <base-url> <relative-url> "
 	 << endl;
     exit(1);
   }
@@ -95,23 +115,31 @@ int main(int argc, char ** argv) {
   url = *(argv + 1);
 
   relUrl = *(argv + 2);
+  if(static_cast<int>(*relUrl) == POUND) {
+    cout << url
+         << relUrl;
+    return 1;
+  }
 
   char * root;
   root = url;
-
-  url = removeRoot(url);
+  
+  url = removeRoot(url);  
   if(static_cast<int> (*relUrl) == SLASH) {
     cout << root
 	 << relUrl
 	 << endl;
-  } else {
-    relUrl = resolveUrl(url,relUrl);
+  } else {  
     cout << root
-	 << '/'
-	 << url
-	 << '/'
-	 << relUrl
-	 << endl;
+         << '/';
+    relUrl = resolveUrl(url,relUrl);    
+    if ((*url) != NUL) {
+      cout << url
+	   << '/';
+    }    
+    cout << relUrl
+         << endl;
   }
   return 1;
 }
+
